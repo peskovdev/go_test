5ea7676533b53a85194052cf508cdc7c86adc678
Peskov Sergey <peskovdev@proton.me>
2023-11-17 02:34:31 +0600

    Add random formatting

--- greetings.go
+++ greetings.go
@@ -3,12 +3,22 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if len(name) == 0 {
 		return "", errors.New("Empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Greate to see you, %v",
+		"Gratias %v. Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
 }
